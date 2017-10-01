a706355b1c0a4ceeaf1b2bae613717aee6574c2b
Derry Hamilton <derryh@rasilon.net>
2017-10-01 17:47:53 +0100

    Initial checkin

---
+++ main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main.c
+ * Author: derryh
+ *
+ * Created on 29 August 2017, 20:05
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=PropsLighting.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/PropsLighting.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=propslighting.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/propslighting.x.tar
